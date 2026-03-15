0f3506c6872714e96773c21a17049eee0f8feb7a
Jakub668 <jakub.hunek19@gmail.com>
2026-03-15 15:46:13 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button1)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Siema z aplikacji!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
